8fe100d002be006005bdbaab6b1c3352edcc2b31
adrii0113 <adrian190401@gmail.com>
2022-06-17 16:24:53 +0200

    first folder structure changes

---
+++ ecommerce/src/Components/Structure/Topbar/topbar.jsx
@@ -0,0 +1,19 @@
+export default function Topbar(){
+
+    return(
+        <div className="topbar-main-container">
+            <div className="logo-container">
+                <img src="./../assets/logos/logo.svg" alt="" />
+            </div>
+
+            <div className="search-something-container">
+
+            </div>
+
+            <div className="menu-container">
+
+            </div>
+        </div>
+    )
+
+}
